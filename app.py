68cfa87a7610273e6955cf10a6446c2605ad70af
Gabe <gabea575@gmail.com>
2025-11-09 18:10:01 -0500

    cahnge debug to false in main branch

--- app.py
+++ app.py
@@ -559,4 +559,4 @@ import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render provides PORT
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
